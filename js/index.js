23dd242bcf703b3937399e9cf532706fb2e1600b
Yurii04UA <uyriparaka@gmail.com>
2022-03-28 02:25:36 +0300

    js added

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function onEntry( entry) {
+   entry.forEach( change => {
+      if( change.isIntersecting){
+         change.target.classList.add('element_show')
+      }
+   })
+}
+
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.card');
+for (let elm of elements) {
+  observer.observe(elm);
+}
