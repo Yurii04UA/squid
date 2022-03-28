018bad251108e65d9c1b9d7d72ce1913cf079011
Yurii04UA <uyriparaka@gmail.com>
2022-03-28 18:37:27 +0300

    added animation for footer

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
-
+// show main cards
 function onEntry( entry) {
    entry.forEach( change => {
       if( change.isIntersecting){
-         change.target.classList.add('element_show')
+         change.target.classList.add('element_show') // added class for main cards
+         change.target.classList.add('show_card') // added class for footer cards
       }
       console.log(change);
    })
 }
 
-// show cards left
+// show main cards left
 let optionsLeft = { threshold: [0.5] };
 let observerLeft = new IntersectionObserver(onEntry, optionsLeft);
 let elementsLeft = document.querySelectorAll('.hiden_left');
@@ -16,7 +17,7 @@ for (let elm of elementsLeft) {
    observerLeft.observe(elm);
 }
 
-// show cards right
+// show main cards right
   
 let optionsRight = { threshold: [0.5] };
 let observerRight = new IntersectionObserver(onEntry, optionsRight);
@@ -24,3 +25,12 @@ let elementsRight = document.querySelectorAll('.hiden_right');
 for (let elm of elementsRight) {
   observerRight.observe(elm);
 }
+
+// show footer cards 
+let optionsFooter = { threshold: [0.5] };
+let observerFooter = new IntersectionObserver(onEntry, optionsFooter);
+let elementsFooter = document.querySelectorAll('.hiden_subscription_card');
+for (let elm of elementsFooter) {
+   observerFooter.observe(elm);
+}
+
