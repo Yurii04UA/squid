3e4620d063b56d67642275dc1ed3f954b90b108c
Yurii04UA <uyriparaka@gmail.com>
2022-03-28 18:22:17 +0300

    added animations

--- js/index.js
+++ js/index.js
@@ -1,16 +1,26 @@
+
 function onEntry( entry) {
    entry.forEach( change => {
       if( change.isIntersecting){
          change.target.classList.add('element_show')
       }
+      console.log(change);
    })
 }
 
-let options = { threshold: [0.5] };
-let observer = new IntersectionObserver(onEntry, options);
-let elements = document.querySelectorAll('.card');
-for (let elm of elements) {
-  observer.observe(elm);
+// show cards left
+let optionsLeft = { threshold: [0.5] };
+let observerLeft = new IntersectionObserver(onEntry, optionsLeft);
+let elementsLeft = document.querySelectorAll('.hiden_left');
+for (let elm of elementsLeft) {
+   observerLeft.observe(elm);
 }
 
-// need added Animation for all cards  
+// show cards right
+  
+let optionsRight = { threshold: [0.5] };
+let observerRight = new IntersectionObserver(onEntry, optionsRight);
+let elementsRight = document.querySelectorAll('.hiden_right');
+for (let elm of elementsRight) {
+  observerRight.observe(elm);
+}
